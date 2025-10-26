411a20ba4c49f5c04e6c8843afdf63be448c791b
SunilKotte <sunilcoviet@gmail.com>
2025-10-26 12:00:53 -0500

    Extending out Token and Lexer we will add support for ==, !, !=, -, /, *, <, > and the keywords true, false, if, else and return.

--- token/token.go
+++ token/token.go
@@ -15,8 +15,17 @@ const (
 	IDENT = "IDENT" //add, foobar, x, y
 	INT   = "INT"   //123456
 	// Operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	ASTERISK = "*"
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	SLASH    = "/"
+	LT       = "<"
+	GT       = ">"
+	EQ       = "=="
+	NOT_EQ   = "!="
+
 	// Delimiters
 	COMMA     = ","
 	SEMICOLON = ";"
@@ -27,14 +36,28 @@ const (
 	// Keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TURE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	ELSEIF   = "ELSEIF"
+	RETURN   = "RETURN"
 )
 
 var keywords = map[string]TokenType{
-	"fn" : FUNCTION,
-	"let":LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"elseIf": ELSEIF,
+	"return": RETURN,
 }
 
-func LookupIdent(ident string) TokenType{
-if tok, ok := keywords[ident];ok{return tok}
-return IDENT
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
 }
